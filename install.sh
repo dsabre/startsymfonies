267d890be0d98da31b58aeee01cbf2d070049dc8
Daniele Sabre <daniele.sabre@gmail.com>
2017-09-28 15:20:48 +0200

    Added installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+composer install
+
+php bin/console doctrine:schema:create
+
+php bin/console cache:clear
+
+php bin/console assets:install --symlink
+
+echo
+echo 'SCANNING DIRECTORIES...'
+
+php bin/console app:symfonies:scan
+
+echo 'Startsymfonies2 successfully installed :)'
