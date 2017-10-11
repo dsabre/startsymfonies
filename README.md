b1b72e0b5fae98b126a46aea3c5ecf82a853025b
Daniele Sabre <daniele.sabre@gmail.com>
2017-10-11 11:35:11 +0200

    Improved installation command

--- README.md
+++ README.md
@@ -5,17 +5,20 @@ One Symfony to rule them all, One Symfony to find them, One Symfony to bring the
 1) Clone or download this repository and put where you want under your home directory.
 2) Use terminal to enter in the startsymfonies2 directory.
 3) Launch the `./install.sh` to install the project, (if you don't have composer, get it here: https://getcomposer.org/).
-4) Launch the command `php bin/console server:start 127.0.0.1:8000` to start the symfony.
-5) OPTIONAL: for a more automated experience put the following two rows in your crontab file:<br>
-- Auto-start the startsymfonies2, 12 seconds after the boot of pc:<br>
-`@reboot sleep 12 && /path/of/php /home/username/your_beautiful_project_directory/startsymfonies2/bin/console server:start 127.0.0.1:8000`
-- Auto-start all symfonies with an ip and a port, 15 seconds after the boot of pc:<br>
-`@reboot sleep 15 && /path/of/php /home/username/your_beautiful_project_directory/startsymfonies2/bin/console app:symfonies:start-all`<br>
 
 ## Description
 This symfony is used to manage all other symfonies found in the directories indicated in installation phase.
 
 It create a table in the homepage of the site that indicate all symfonies found and their status, it provide a link to fast access the running symfony.
+
+## Useful commands
+To run startsymfonies2 use:<br>`php bin/console server:start 127.0.0.1:8000`
+
+To auto-run the startsymfonies2, 12 seconds after the boot of pc:<br>
+`@reboot sleep 12 && /path/of/php /home/username/your_beautiful_project_directory/startsymfonies2/bin/console server:start 127.0.0.1:8000`
+
+To auto-run all symfonies with an ip and a port, 15 seconds after the boot of pc:<br>
+`@reboot sleep 15 && /path/of/php /home/username/your_beautiful_project_directory/startsymfonies2/bin/console app:symfonies:start-all`
 
 #### Flux
 1) When a symfony is found it will be inserted in a sqlite database created in the project directory at path `var/data/data.sqlite`.
--- install.sh
+++ install.sh
@@ -1,17 +1,64 @@
 #!/usr/bin/env bash
 
+# installing symfony
 composer install
 
-php bin/console doctrine:schema:create
+# variable definition
+baseIp="127.0.0.1"
+basePort="8000"
+databasePath=$(cat app/config/parameters.yml |grep database_path | awk -F':' '{print $2}' |sed "s/'//g" |sed "s/%kernel.project_dir%\///g" |sed 's/^ //g' |sed 's/ $//g')
+phpExecutable=$(cat app/config/parameters.yml |grep php_executable | awk -F':' '{print $2}' |sed "s/'//g" |sed 's/^ //g' |sed 's/ $//g')
+currentDir=$(pwd)
+lineAutoStart="@reboot sleep 12 && $phpExecutable $currentDir/bin/console server:start $baseIp:$basePort"
+lineAutoStartSymfonies="@reboot sleep 15 && $phpExecutable $currentDir/bin/console app:symfonies:start-all"
+lineAutoStartExist=$(crontab -l |grep "$lineAutoStart" |wc -l)
+lineAutoStartSymfoniesExist=$(crontab -l |grep "$lineAutoStartSymfonies" |wc -l)
+serverRunning=$($(echo $phpExecutable) bin/console server:status -q;echo $?)
 
-php bin/console cache:clear
+# create database if not exist
+if [ ! -e "$databasePath" ]; then
+    $(echo $phpExecutable) bin/console doctrine:schema:create
+fi
 
-php bin/console assets:install --symlink
+# perform base commands for correct working
+$(echo $phpExecutable) bin/console cache:clear
+$(echo $phpExecutable) bin/console assets:install --symlink
+
+# check if user want to perform a scan now
+echo
+read -p "Do you want to perform a scan for symfonies now [y/N]? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo
+    echo 'SCANNING DIRECTORIES...'
+    $(echo $phpExecutable) bin/console app:symfonies:scan
+    echo 'SCAN COMPLETED'
+fi
+
+# check if user want to autostart startsymfonies2 on pc boot
+if [ "$lineAutoStartExist" -eq "0" ]; then
+    echo
+    read -p "Do you want to run startsymfonies2 on pc boot [y/N]? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        (crontab -l; echo; echo "# STARTSYMFONIES 2 - AUTOSTART"; echo "$lineAutoStart") |crontab -
+    fi
+fi
+
+# check if user want to autostart symfonies with an ip and a port on pc boot
+if [ "$lineAutoStartSymfoniesExist" -eq "0" ]; then
+    echo
+    read -p "Do you want to run all symfonies with an ip and a port on pc boot [y/N]? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        (crontab -l; echo; echo "# STARTSYMFONIES 2 - AUTOSTART SYMFONIES"; echo "$lineAutoStartSymfonies") |crontab -
+    fi
+fi
+
+# run startsymfonies2 now if not running
+if [ "$serverRunning" -eq "1" ]; then
+    $(echo $phpExecutable) bin/console server:start $baseIp:$basePort
+fi
 
 echo
-echo 'SCANNING DIRECTORIES...'
-php bin/console app:symfonies:scan
-echo 'SCAN COMPLETED'
-
 echo 'Startsymfonies2 successfully installed :)'
 echo
+
+exit 0
