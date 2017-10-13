3a0370815e73a6dc80877d34d661d5755969bae7
Daniele Sabre <daniele.sabre@gmail.com>
2017-10-13 09:53:44 +0200

    Code improvement

---
+++ src/AppBundle/Resources/public/scripts/search.js
@@ -0,0 +1,38 @@
+$(function(){
+	const searchField    = $('#search');
+	const flgHideStopped = $("#flgHideStopped");
+	
+	manageRows();
+	
+	$("#clearSearch").click(function(e){
+		e.preventDefault();
+		
+		searchField.val('');
+		manageRows();
+		
+		return false;
+	});
+	
+	flgHideStopped.click(function(){
+		manageRows();
+	});
+	
+	searchField.keyup(function(){
+		manageRows();
+	});
+	
+	function manageRows(){
+		var string      = $.trim(searchField.val()).replace(/ +/g, " ").toLowerCase();
+		var hideSkipped = flgHideStopped.is(":checked");
+		
+		$('table tbody tr').show().filter(function(){
+			var text   = $(this).text().replace(/\s+/g, " ").toLowerCase();
+			var status = $(this).children("td.colLink").text().replace(/\s+/g, " ").toLowerCase();
+			
+			var checkString  = string !== "" ? !~text.indexOf(string) : false;
+			var checkSkipped = hideSkipped ? ~status.indexOf("stopped") : false;
+			
+			return checkString || checkSkipped;
+		}).hide();
+	}
+});
