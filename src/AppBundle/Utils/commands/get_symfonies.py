216a0b41fc3967d5a947d16eef4553ee84ec86b5
Daniele Sabre <daniele.sabre@gmail.com>
2017-10-23 10:27:33 +0200

    Bugfix in python program

--- src/AppBundle/Utils/commands/get_symfonies.py
+++ src/AppBundle/Utils/commands/get_symfonies.py
@@ -28,11 +28,11 @@ for dirname, dirnames, filenames in os.walk(d):
 			ver = subprocess.Popen(["php", symfony2], stdout=subprocess.PIPE, stderr=FNULL).communicate()[0].split('\n')[0].split(' - ')[0].split(' 2')[1]
 			print symfony2 + '|2' + ver
 		except: 
-			pass
+			print symfony2 + '|2.x.x'
 		
 	if isSymfony3:
 		try:
 			ver = subprocess.Popen(["php", symfony3], stdout=subprocess.PIPE, stderr=FNULL).communicate()[0].split('\n')[0].split(' (')[0].split(' 3')[1]
 			print symfony3 + '|3' + ver
 		except: 
-			pass
+			print symfony2 + '|3.x.x'
