e21d4f967665a59c022cfb6b1e032cec41edc43c
Daniele Sabre <daniele.sabre@gmail.com>
2018-09-13 11:04:03 +0200

    Improved composer show management

--- src/AppBundle/Resources/public/scripts/composerShow.js
+++ src/AppBundle/Resources/public/scripts/composerShow.js
@@ -1,23 +1,29 @@
 $(function() {
     $('.composerShow').click(function(e){
-		const symfonyId = parseInt($.trim($(this).data('symfonyid')));
-		const elmEmpty = $('#modalComposerShow' + symfonyId + '-empty');
+		var symfonyId = parseInt($.trim($(this).data('symfonyid')));
+		var path = $.trim($(this).data('path'));
+		var elmEmpty = $('#modalComposerShow-empty');
+		var elmContent = $('#modalComposerShow-content');
 	
-		if(elmEmpty.length > 0){
-			$.ajax({
-				url: '/composer/show/' + symfonyId,
-				success: function(response){
-					var output = '';
-					var color = false;
-					
-					$.each(response, function(k, row){
-						output += '<span ' + (color ? 'class="text-info"' : '') + '> ' + row + ' </span><br>';
-						color = !color;
-					});
-					
-					elmEmpty.after('<small><pre>' + output + '</pre></small>').remove();
-				}
-			});
-		}
+		elmEmpty.removeClass('d-none');
+		elmContent.text('');
+	
+		$('#modalComposerShowPath').text(path);
+	
+		$.ajax({
+			url: '/composer/show/' + symfonyId,
+			success: function(response){
+				var output = '';
+				var color = false;
+			
+				$.each(response, function(k, row){
+					output += '<small ' + (color ? 'class="text-info"' : '') + '> ' + row + ' </small><br>';
+					color = !color;
+				});
+			
+				elmEmpty.addClass('d-none');
+				elmContent.html(output);
+			}
+		});
 	});
 });
